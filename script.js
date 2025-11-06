8c1af58a334c42f736f61528af759967104de542
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-11-06 14:53:52 +0000

    quase terminando

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //Seleciona os elementos das luzes
-const luzVermelha = document.getElemontById("vermelho");
-const luzAmarela = document.getElemontById("amarelo");
-const luzVerde = document.getElemontById("verde");
+const luzVermelha = document.getElementById("vermelho");
+const luzAmarelo = document.getElementById("amarela");
+const luzVerde = document.getElementById("verde");
 // Botões
 const btnVermelho = document.getElementById("btnVermelho");
-const btnAmarelo = document.getElementById("btnAmarelo");
+const btnAmarela = document.getElementById("btnAmarela");
 const btnVerde = document.getElementById("btnVerde");
 const btnAuto = document.getElementById("btnAuto");
 const btnParar = document.getElementById("btnParar");
@@ -13,13 +13,42 @@ let intervalo;
 
 function limpar(){
     luzVermelha.className = 'luz';
-    luzAmarela.className = 'luz';
+    luzAmarelo.className = 'luz';
     luzVerde.className = 'luz';
 }
+
 function acenderVermelho(){
     limpar();
-    luzVermelha.classList.add("acesa","vermelha");
+    luzVermelha.classList.add("acesa","vermelho");
 }
 
-function acenderAmarelo(){
-    
+function acenderAmarela(){
+    limpar();
+    luzAmarelo.classList.add("acesa","amarela");
+}
+
+function acenderVerde(){
+    limpar();
+    luzVerde.classList.add("acesa","verde");
+}
+
+function modoAutomativo(){
+    limpar();
+    let estado =0
+    intervalo = setInterval(()=>{
+        if(estado === 0 ) acenderVermelho();
+        else if (estado === 1 ) acenderAmarela();
+        else if (estado === 1 ) acenderVerde();
+        estado = (estado +1) % 3;
+    },1000)
+}
+
+function parar(){
+    clearInterval(intervalo);
+    limpar();
+}
+btnParar.onclick = parar;
+btnAuto.onclick = modoAutomativo;
+btnVerde.onclick = acenderVerde;
+btnAmarelo.onclick = acenderAmarela;
+btnVermelho.onclick = acenderVermelho;
